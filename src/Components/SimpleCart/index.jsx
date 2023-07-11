dbf13310017a82633f4b53f771ef10f82decafae
Kenya Womack <ladykamillion27@gmail.com>
2023-07-11 17:16:15 -0500

    update

--- src/Components/SimpleCart/index.jsx
+++ src/Components/SimpleCart/index.jsx
@@ -1,12 +1,19 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { When } from 'react-if';
-import { remove } from '../../store/actions';
+import { removeFromCart } from '../../store/cart';
+import { incrementInventoryOnRemove } from '../../store/products';
 
 import './styles.scss';
 
 function SimpleCart() {
-  const { cart } = useSelector(state => state.cart);
+  const { cart } = useSelector(state => state);
   const dispatch = useDispatch();
+
+  const removeDispatcher = (product) => {
+    dispatch(removeFromCart(product));
+    dispatch(incrementInventoryOnRemove(product));
+  };
+
   return (
     <>
       <When condition={cart.length > 0}>
@@ -18,7 +25,7 @@ function SimpleCart() {
                 <li key={`simpleCart-${index}`} className="item">
                   {product.name}
                   <span
-                    onClick={() => dispatch(remove(product))}
+                    onClick={() => removeDispatcher(product)}
                     className="remove"
                   >
                     X
